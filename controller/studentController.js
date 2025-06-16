fb88a63eda2e206739da7f1eb69069f2d939537c
ProoofAdmin <admin@prooof.in>
2025-06-16 16:46:23 +0530

    Added addStudent API

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import Student from "../models/student.js";
 
 export default ({db}) =>{
   let api = Router();
 
   api.post('/addStudent', (req,res)=> {
     const { studentId, name, status } = req.body;
-    
-
+    const student = new Student({studentId, name, status});
+    const response = student.save();
+    return res.status(200).json(response);
   });
 
   return api;
